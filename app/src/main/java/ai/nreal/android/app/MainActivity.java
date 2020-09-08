514dbdb9f2fab0d07af3fa863e7a51b71d197434
wangzhijiang <zjwang@nreal.ai>
2020-09-08 15:48:39 +0800

    Initialize an empty android project.

---
+++ app/src/main/java/ai/nreal/android/app/MainActivity.java
@@ -0,0 +1,14 @@
+package ai.nreal.android.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
